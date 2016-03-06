5e3f997bbddf171304f9915cadb1494d98103479
hornoo <rchrdhorne1@gmail.com>
2016-03-07 09:51:01 +1300

    Count_ofAStackWith4Item_Returns4
    testing count with 4 items, tested ok

--- StackStringPractical/UnitTestStackClass/UnitTest1.cs
+++ StackStringPractical/UnitTestStackClass/UnitTest1.cs
@@ -41,5 +41,28 @@ namespace UnitTestStackClass
             Assert.AreEqual(oneCount, result);
 
         }
+
+        [TestMethod]
+        public void Count_ofAStackWith4Item_Returns4()
+        {
+            Stack testStack = new Stack();
+
+            StringNode testStringOne = new StringNode("this is a testString 1");
+            StringNode testStringTwo = new StringNode("this is a testString 2");
+            StringNode testStringThree = new StringNode("this is a testString 3");
+            StringNode testStringFour = new StringNode("this is a testString 4");
+
+            testStack.push(testStringOne);
+            testStack.push(testStringTwo);
+            testStack.push(testStringThree);
+            testStack.push(testStringFour);
+
+            int oneCount = 4;
+
+            int result = testStack.Count();
+
+            Assert.AreEqual(oneCount, result);
+
+        }
     }
 }
