00edc8fb3e3b31175b9b88034b8108ee872a05e7
hornoo <rchrdhorne1@gmail.com>
2016-03-06 22:14:58 +1300

    added in standard stack  methods

--- StackStringPractical/StackStringPractical/Stack.cs
+++ StackStringPractical/StackStringPractical/Stack.cs
@@ -8,5 +8,39 @@ namespace StackStringPractical
 {
     class Stack
     {
+        List<String> stackStringlist;
+
+        public Stack()
+        {
+            stackStringlist = new List<String>();
+        }
+
+        public void push(string newString)
+        {
+
+        }
+
+        public String Pop() 
+        {
+            return null;
+        }
+
+        public String Peak()
+        {
+            return null;
+        }
+
+        public int Count()
+        {
+            return 1;
+        }
+
+        public bool isEmpty() 
+        {   
+
+            return false;
+        }
+
+     
     }
 }
--- StackStringPractical/UnitTestStackClass/UnitTest1.cs
+++ StackStringPractical/UnitTestStackClass/UnitTest1.cs
@@ -10,5 +10,7 @@ namespace UnitTestStackClass
         public void TestMethod1()
         {
         }
+
+
     }
 }
