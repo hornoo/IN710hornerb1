8f971524e848159a1030671bc4c2f57d83ffc04a
hornoo <rchrdhorne1@gmail.com>
2016-03-08 20:42:06 +1300

    Added Knight child class
    Declaim method added

---
+++ Game Character Skeleton/Game Character/Knight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public class Knight : Character
+    {
+        public override string Declaim()
+        {
+            return "I am a chivalrous Knight!";
+        }
+    }
+}
