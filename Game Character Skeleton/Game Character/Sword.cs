286b24ea5bc8164ab690b1fb76cdc58d03167043
hornoo <rchrdhorne1@gmail.com>
2016-03-08 21:04:11 +1300

    Added sword class

---
+++ Game Character Skeleton/Game Character/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public class Sword : IWeapon
+    {
+        public string UseWeapon()
+        {
+            return "I slice with my sword!";
+        }
+    }
+}
