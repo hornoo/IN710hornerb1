0957295ed4dae094c2bb885ba1475803e2f6e463
hornoo <rchrdhorne1@gmail.com>
2016-03-08 15:01:32 +1300

    Initial commit of game charater

---
+++ Game Character Skeleton/Game Character/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    class Character
+    {
+        protected String charaterName;
+
+        public String Statename()
+        {
+            return "My name is " + charaterName + "!";
+        }
+
+        public abstract String Declare();
+    }
+}
---
+++ Game Character Skeleton/Game Character/King.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    class King : Character
+    {
+
+        public override string Declare()
+        {
+            throw new NotImplementedException();
+        } 
+
+
+    }
+}
