31688f650536d2869b23250ccf32cddef7627b18
hornoo <rchrdhorne1@gmail.com>
2016-03-08 20:44:05 +1300

    added Troll child class
    declaim method

---
+++ Game Character Skeleton/Game Character/Troll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public class Troll : Character
+    {
+        public override string Declaim()
+        {
+            return "Trolls don't have time to chat!";    
+        }
+    }
+}
