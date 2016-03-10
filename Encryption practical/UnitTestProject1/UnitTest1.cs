2a6814488e094dd6668fbcffae3c4967e49805c9
hornoo <rchrdhorne1@gmail.com>
2016-03-10 13:55:31 +1300

    Added a few more tests
    Tested ok

--- Encryption practical/UnitTestProject1/UnitTest1.cs
+++ Encryption practical/UnitTestProject1/UnitTest1.cs
@@ -30,5 +30,44 @@ namespace UnitTestProject1
 
             Assert.AreEqual(expectedtext, resulttext);
         }
+
+
+        [TestMethod]
+        public void ReverseEncrypt_encryptAStringAndNumbers_encryptedString()
+        {
+            ReverseEncrption encryptmachine = new ReverseEncrption();
+
+            String plaintext = "Richard 13 Horne 01";
+            String expectedtext = "10 enroH 31 drahciR";
+            String resulttext = encryptmachine.EncryptDecrypt(plaintext);
+
+            Assert.AreEqual(expectedtext, resulttext);
+        }
+
+
+        [TestMethod]
+        public void Rot13_encryptAStringAndNumbers_encryptedString()
+        {
+            ROT13Encryt encryptmachine = new ROT13Encryt();
+
+            String plaintext = "Richard 13 Horne 01";
+            String expectedtext = "Evpuneq 13 Ubear 01";
+            String resulttext = encryptmachine.EncryptDecrypt(plaintext);
+
+            Assert.AreEqual(expectedtext, resulttext);
+        }
+
+        [TestMethod]
+        public void Rot13_DecryptAStringAndNumbers_encryptedString()
+        {
+            ROT13Encryt encryptmachine = new ROT13Encryt();
+
+            String encyptedText = "Evpuneq 13 Ubear 01";
+            String expectedtext = "Richard 13 Horne 01";
+            String resulttext = encryptmachine.EncryptDecrypt(encyptedText);
+
+            Assert.AreEqual(expectedtext, resulttext);
+        }
+
     }
 }
