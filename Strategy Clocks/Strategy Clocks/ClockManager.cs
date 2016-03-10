f2f64c8c8b1d048019beb1a5a437bbf6c703f163
hornoo <rchrdhorne1@gmail.com>
2016-03-10 15:21:22 +1300

    smoko

---
+++ Strategy Clocks/Strategy Clocks/ClockManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Clocks
+{
+    public class ClockManager
+    {
+        public IClock ClockMachine {get; set;}
+
+        public ClockManager()
+        {
+
+        }
+
+        public void StartClock() 
+        {
+
+        }
+
+        public void StopClock()
+        {
+
+        }
+
+
+
+    }
+}
