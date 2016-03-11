4639300057dedb836777316fc314b4efc9eaa848
hornoo <rchrdhorne1@gmail.com>
2016-03-11 14:35:43 +1300

    Fixed corrupt testes, added 3 more

--- Game Character Skeleton/UnitTestProject1/UnitTest1.cs
+++ Game Character Skeleton/UnitTestProject1/UnitTest1.cs
@@ -8,11 +8,52 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void ToString_CharactedThatsDefaultInstationtion_returnsTostringWithDefaultWeapon()
+        public void ToStringKing_CharactedThatsDefaultInstationtion_returnsTostringWithDefaultWeapon()
         {
             Character testCharacter = new King("Bob");
 
             String expectedString = "My name is Bob! I am the most mighty King! I slice with my sword!";
+
+            String actualString = testCharacter.ToString();
+
+            Assert.AreEqual(expectedString, actualString);
+
+        }
+
+
+        [TestMethod]
+        public void ToStringQueen_CharactedThatsDefaultInstationtion_returnsTostringWithDefaultWeapon()
+        {
+            Character testCharacter = new Queen("Bob");
+
+            String expectedString = "My name is Bob! I am the powerful Queen! I shoot with my bow!";
+
+            String actualString = testCharacter.ToString();
+
+            Assert.AreEqual(expectedString, actualString);
+
+        }
+
+
+        [TestMethod]
+        public void ToStringTroll_CharactedThatsDefaultInstationtion_returnsTostringWithDefaultWeapon()
+        {
+            Character testCharacter = new Troll("Bob");
+
+            String expectedString = "My name is Bob! Trolls don't have time to chat! I shoot with my bow!";
+
+            String actualString = testCharacter.ToString();
+
+            Assert.AreEqual(expectedString, actualString);
+
+        }
+
+        [TestMethod]
+        public void ToStringKnight_CharactedThatsDefaultInstationtion_returnsTostringWithDefaultWeapon()
+        {
+            Character testCharacter = new Knight("Bob");
+
+            String expectedString = "My name is Bob! I am a chivalrous Knight! I Stab with my knife!";
 
             String actualString = testCharacter.ToString();
 
