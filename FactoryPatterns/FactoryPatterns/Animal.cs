a26229cc3b8f339473df202ea537b6e4ca5ea038
hornoo <rchrdhorne1@gmail.com>
2016-03-15 15:47:31 +1300

    added animal parent class

---
+++ FactoryPatterns/FactoryPatterns/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FactoryPatterns
+{
+    public abstract class Animal
+    {
+        protected String speciesName;
+        protected String feedingBehaviour;
+        protected String mainFoodType;
+
+        public Bitmap image { get; }
+
+        public override string ToString()
+        {
+            String returnString = "I'm " + speciesName + ".";
+            returnString += " I am a " + feedingBehaviour + ".";
+            returnString += " I eat " + mainFoodType + ".";
+
+            return returnString;
+        }
+    }
+}
