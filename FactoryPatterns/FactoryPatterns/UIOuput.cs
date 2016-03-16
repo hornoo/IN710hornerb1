ae99f547fa2a16bb3dc3b081adcc74f1306cbe6d
hornoo <rchrdhorne1@gmail.com>
2016-03-16 23:18:44 +1300

    added ui output method and code

--- FactoryPatterns/FactoryPatterns/UIOuput.cs
+++ FactoryPatterns/FactoryPatterns/UIOuput.cs
@@ -10,10 +10,34 @@ namespace FactoryPatterns
 {
    public class UIOuput
     {
+       const int INIIAL_IMAGE_XPOS = 20;
+       const int INITIAL_IMAGE_YPOS = 20;
+       Graphics canvas;
+       ListBox outPutListbox;
+
        public UIOuput(Graphics canvas, ListBox outPutListbox )
        {
+           this.canvas = canvas;
+           this.outPutListbox = outPutListbox;
+       }
+
+
+       public void drawAndlistAnimal(List<Animal> inputAnimalList)
+       {
+           int xpos = INIIAL_IMAGE_XPOS;
+           int ypos = INITIAL_IMAGE_YPOS;
+
+           foreach(Animal currentAnimal in inputAnimalList)
+           {
+               canvas.DrawImage(currentAnimal.Image,xpos,ypos);
+               outPutListbox.Items.Add(currentAnimal.ToString());
+               ypos += currentAnimal.Image.Height;
+
+           }
 
        }
+
+
 
 
     }
