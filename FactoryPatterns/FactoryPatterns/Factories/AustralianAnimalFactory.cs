44246742f0494949930f08ef32d275e07be5d201
hornoo <rchrdhorne1@gmail.com>
2016-03-16 21:56:25 +1300

    completed initial Aus animal factory

--- FactoryPatterns/FactoryPatterns/Factories/AustralianAnimalFactory.cs
+++ FactoryPatterns/FactoryPatterns/Factories/AustralianAnimalFactory.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace FactoryPatterns.Factories
 {
-    class AustralianAnimalFactory
+    public class AustralianAnimalFactory : IAnimalFactory
     {
+        public Animal createAnimal(int animalCode)
+        {
+            Animal newAnimal = null;
+
+            switch (animalCode)
+            {
+                case 0:
+                    newAnimal = new Crocodile();
+                    break;
+                case 1:
+                    newAnimal = new Koala();
+                    break;
+                case 2:
+                    newAnimal = new Tasmaniandevil();
+                    break;
+            }
+
+
+
+            return newAnimal;
+        }
     }
 }
