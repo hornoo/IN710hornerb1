70d8ecd38a015b4f18650efa0465b7d77603be79
hornoo <rchrdhorne1@gmail.com>
2016-03-16 22:43:00 +1300

    added UIoutput class

--- FactoryPatterns/FactoryPatterns/Form1.cs
+++ FactoryPatterns/FactoryPatterns/Form1.cs
@@ -12,9 +12,29 @@ namespace FactoryPatterns
 {
     public partial class Form1 : Form
     {
+        Graphics formCanvas;
+        Random randomIntGenetator;
+        NorthAmerica northAmericaCont;
+        Australia australiaCont;
+        UIOuput UIOutWorker;
+
         public Form1()
         {
             InitializeComponent();
+            formCanvas = CreateGraphics();
+            randomIntGenetator = new Random();
+            northAmericaCont = new NorthAmerica(3, randomIntGenetator);
+            australiaCont = new Australia(3, randomIntGenetator);
+        }
+
+        private void btAuz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btUSA_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ FactoryPatterns/FactoryPatterns/UIOuput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms
+
+namespace FactoryPatterns
+{
+   public class UIOuput
+    {
+       public UIOuput(Graphics canvas, ListBox outPutListbox )
+       {
+
+       }
+
+
+    }
+}
