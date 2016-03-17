e5c8a6ac4e9b8e426f842cb0df116e2a65210685
hornoo <rchrdhorne1@gmail.com>
2016-03-17 14:54:39 +1300

    add images to test, still cant get test working

---
+++ FactoryPatterns/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FactoryPatterns;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Kandaroo_FromanimalFactory_kangarooObject()
+        {
+            AustralianAnimalFactory testFact = new AustralianAnimalFactory();
+
+            Kangaroo testRoo = new Kangaroo();
+
+            Animal testAnimal = null;
+
+            testAnimal = testFact.createAnimal(3);
+
+            Assert.AreEqual(testAnimal, testRoo);
+
+        }
+    }
+}
