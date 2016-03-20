2661792cf1f465ba099c22851b33af9c16b5d018
hornoo <rchrdhorne1@gmail.com>
2016-03-21 11:26:11 +1300

    Fairy and frank factory

---
+++ MonsterFactory/MonsterFactory/Factorys/FairyFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterFactory
+{
+    public class FairyFactory : IMonsterFactory
+    {
+        public FairyFactory()
+        {
+
+        }
+
+        public Head getMonsterhead()
+        {
+            return new FairyHead();
+        }
+
+        public Body getMonsterBody()
+        {
+            return new FairyBody();
+        }
+
+        public Legs getMonsterLegs()
+        {
+            return new FairyLegs();
+        }
+    }
+}
---
+++ MonsterFactory/MonsterFactory/Factorys/FrankensteinFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterFactory
+{
+    public class FrankensteinFactory : IMonsterFactory
+    {
+        public FrankensteinFactory()
+        {
+
+        }
+
+        public Head getMonsterhead()
+        {
+            return new FrankHead();
+        }
+
+        public Body getMonsterBody()
+        {
+            return new FrankBody();
+        }
+
+        public Legs getMonsterLegs()
+        {
+            return new FrankLegs();
+        }
+    }
+}
