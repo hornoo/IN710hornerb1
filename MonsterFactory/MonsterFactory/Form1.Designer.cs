d77841317d27136dc470b3ebba2c0f699ea164a0
hornoo <rchrdhorne1@gmail.com>
2016-03-21 11:50:17 +1300

    working on form logic

--- MonsterFactory/MonsterFactory/Form1.Designer.cs
+++ MonsterFactory/MonsterFactory/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbhead = new System.Windows.Forms.PictureBox();
+            this.pbbody = new System.Windows.Forms.PictureBox();
+            this.pbLegs = new System.Windows.Forms.PictureBox();
+            this.cbhead = new System.Windows.Forms.ComboBox();
+            this.cbbody = new System.Windows.Forms.ComboBox();
+            this.cblegs = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbhead)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbody)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLegs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbhead
+            // 
+            this.pbhead.Location = new System.Drawing.Point(61, 43);
+            this.pbhead.Name = "pbhead";
+            this.pbhead.Size = new System.Drawing.Size(150, 150);
+            this.pbhead.TabIndex = 0;
+            this.pbhead.TabStop = false;
+            // 
+            // pbbody
+            // 
+            this.pbbody.Location = new System.Drawing.Point(61, 199);
+            this.pbbody.Name = "pbbody";
+            this.pbbody.Size = new System.Drawing.Size(150, 150);
+            this.pbbody.TabIndex = 1;
+            this.pbbody.TabStop = false;
+            // 
+            // pbLegs
+            // 
+            this.pbLegs.Location = new System.Drawing.Point(61, 355);
+            this.pbLegs.Name = "pbLegs";
+            this.pbLegs.Size = new System.Drawing.Size(150, 150);
+            this.pbLegs.TabIndex = 2;
+            this.pbLegs.TabStop = false;
+            // 
+            // cbhead
+            // 
+            this.cbhead.FormattingEnabled = true;
+            this.cbhead.Location = new System.Drawing.Point(331, 43);
+            this.cbhead.Name = "cbhead";
+            this.cbhead.Size = new System.Drawing.Size(146, 21);
+            this.cbhead.TabIndex = 3;
+            // 
+            // cbbody
+            // 
+            this.cbbody.FormattingEnabled = true;
+            this.cbbody.Location = new System.Drawing.Point(331, 107);
+            this.cbbody.Name = "cbbody";
+            this.cbbody.Size = new System.Drawing.Size(146, 21);
+            this.cbbody.TabIndex = 4;
+            // 
+            // cblegs
+            // 
+            this.cblegs.FormattingEnabled = true;
+            this.cblegs.Location = new System.Drawing.Point(331, 172);
+            this.cblegs.Name = "cblegs";
+            this.cblegs.Size = new System.Drawing.Size(146, 21);
+            this.cblegs.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Make Monster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 540);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cblegs);
+            this.Controls.Add(this.cbbody);
+            this.Controls.Add(this.cbhead);
+            this.Controls.Add(this.pbLegs);
+            this.Controls.Add(this.pbbody);
+            this.Controls.Add(this.pbhead);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbhead)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbody)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLegs)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbhead;
+        private System.Windows.Forms.PictureBox pbbody;
+        private System.Windows.Forms.PictureBox pbLegs;
+        private System.Windows.Forms.ComboBox cbhead;
+        private System.Windows.Forms.ComboBox cbbody;
+        private System.Windows.Forms.ComboBox cblegs;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MonsterFactory/MonsterFactory/Form1.cs
+++ MonsterFactory/MonsterFactory/Form1.cs
@@ -12,9 +12,20 @@ namespace MonsterFactory
 {
     public partial class Form1 : Form
     {
+        FairyFactory FairyFactoryWorker;
+        FrankensteinFactory FrankFactoryWorker;
+
         public Form1()
         {
             InitializeComponent();
+            FairyFactoryWorker = new FairyFactory();
+            FrankFactoryWorker = new FrankensteinFactory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
