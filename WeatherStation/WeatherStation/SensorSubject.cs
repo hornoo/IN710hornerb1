d73b757d1152f4f5b6d1a6f992fd31f38409d438
hornoo <rchrdhorne1@gmail.com>
2016-03-24 15:21:02 +1300

    added sensorsubject class

---
+++ WeatherStation/WeatherStation/SensorSubject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherStation
+{
+    class SensorSubject : ISubject
+    {
+        private List<ISensorObserver> observerList;
+        int currentReading;
+
+        public SensorSubject()
+        {
+            observerList = new List<ISensorObserver>();
+            currentReading = 0;
+
+        }
+
+        public void addObserver(ISensorObserver o)
+        {
+            observerList.Add(o);
+        }
+
+        public void removeObserver(ISensorObserver o)
+        {
+            observerList.Remove(o);
+        }
+
+        public void notifyObserver()
+        {
+            foreach (ISensorObserver currentObserver in observerList)
+
+                currentObserver.update(currentReading);
+        }
+
+        public void InputTrigger(string inputString)
+        {
+            try 
+            {
+                currentReading = Int32.Parse(inputString);
+            }
+            catch(FormatException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            notifyObserver();
+
+        }
+    }
+}
