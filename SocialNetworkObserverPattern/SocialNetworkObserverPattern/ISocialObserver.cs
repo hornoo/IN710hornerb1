9fc5b92c90a62d2a378c3cb721595dfa1b79424a
hornoo <rchrdhorne1@gmail.com>
2016-03-27 10:26:18 +1300

    Socialnetwork Observerpattern inital commit

---
+++ SocialNetworkObserverPattern/SocialNetworkObserverPattern/ISocialObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkObserverPattern
+{
+    public interface ISocialObserver
+    {
+        void Update(String statusUpdate);
+        void Display();
+    }
+}
