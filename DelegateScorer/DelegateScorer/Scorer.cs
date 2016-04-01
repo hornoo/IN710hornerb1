affd6c964332e7ac0e134dc3ee58a72329b6a143
hornoo <rchrdhorne1@gmail.com>
2016-04-01 22:18:08 +1300

    static scorer class added with static methods

---
+++ DelegateScorer/DelegateScorer/Scorer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateScorer
+{
+    public static class Scorer
+    {
+        public static int ChildScore(int correct, int incorrect)
+        {
+            int returnValue = (correct * 10) - incorrect;
+
+            return returnValue;
+        }
+
+
+        public static int AdultScore(int correct, int incorrect)
+        {
+            int returnValue = correct * 15;
+
+            return returnValue;
+        }
+
+    }
+}
