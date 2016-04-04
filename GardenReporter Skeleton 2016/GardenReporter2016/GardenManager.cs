4f9359190750e624e1395295204360ead050ba7d
hornoo <rchrdhorne1@gmail.com>
2016-04-04 21:23:09 +1200

    created gardenManager class

---
+++ GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GardenReporter2016
+{
+    public class GardenManager
+    {
+        public delegate String repotDelegate(Garden gardentToProcess);
+
+        List<Garden> gardenList;
+
+        public GardenManager()
+        {
+            gardenList = new List<Garden>();
+        }
+
+        public void AddGarden(Garden garden1)
+        {
+            gardenList.Add(garden1);
+        }
+
+        public List<String> ProcessGardens(repotDelegate inputDel)
+        {
+            List<String> returnList = new List<string>();
+
+            foreach (Garden currentgarden in gardenList)
+            {
+                returnList.Add(inputDel(currentgarden));
+            }
+
+            return returnList;
+        }
+
+
+
+    }
+}
