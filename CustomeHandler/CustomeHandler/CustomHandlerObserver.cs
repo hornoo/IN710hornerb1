d61a002aff43bcd8c43014bc2b0e486e59da5626
hornoo <rchrdhorne1@gmail.com>
2016-04-06 22:49:23 +1200

    customhandler constructor added

--- CustomeHandler/CustomeHandler/CustomHandlerObserver.cs
+++ CustomeHandler/CustomeHandler/CustomHandlerObserver.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CustomeHandler
 {
     class CustomHandlerObserver
     {
+
+        int randomNumber;
+        Button subject;
+
+        public CustomHandlerObserver(Button Subject, int Randomnumber)
+        {
+
+            subject = Subject;
+            randomNumber = Randomnumber
+
+        }
 
     }
 }
