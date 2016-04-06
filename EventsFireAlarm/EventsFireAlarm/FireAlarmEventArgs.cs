f1d85c58ac2a23384eff0b869edc0eb2178b1fe7
hornoo <rchrdhorne1@gmail.com>
2016-04-06 13:03:22 +1200

    create deventargs class

---
+++ EventsFireAlarm/EventsFireAlarm/FireAlarmEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFireAlarm
+{
+    class FireAlarmEventArgs : EventArgs
+    {
+        public String AlertString { get; set; }
+
+        public FireAlarmEventArgs(String inputString)
+        {
+            AlertString = inputString;
+        }
+    }
+}
