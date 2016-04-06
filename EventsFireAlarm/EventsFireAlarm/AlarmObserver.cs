6816e5146be698c5e7f1dd6654ecf7781167ef11
hornoo <rchrdhorne1@gmail.com>
2016-04-06 13:28:28 +1200

    Added alarm observer base class

---
+++ EventsFireAlarm/EventsFireAlarm/AlarmObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFireAlarm
+{
+    public abstract class AlarmObserver
+    {
+        protected AlarmSubject subject;
+
+        protected String alertString;
+
+
+        public AlarmObserver(AlarmSubject Subject)
+        {
+            subject = Subject;
+
+            AlarmSubject.alarmDelegateEventHandler alertMethodDelegate = new AlarmSubject.alarmDelegateEventHandler(AlertMethod);
+
+            subject.fireEvent += alertMethodDelegate;
+
+
+        }
+
+        public abstract void AlertMethod(object o, FireAlarmEventArgs e);
+        
+    }
+}
