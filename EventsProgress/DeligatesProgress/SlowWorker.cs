45497301e41e454e34c18fa06c3055df3f73ae02
hornoo <rchrdhorne1@gmail.com>
2016-04-07 10:11:46 +1200

    set up event in slow worker

---
+++ EventsProgress/DeligatesProgress/SlowWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsProgress
+{
+    public class SlowWorker
+    {
+        
+        public event EventHandler progressEvent;
+
+        public void OnUpdateEvent()
+        {
+            EventArgs pe = new EventArgs();
+
+            if (progressEvent != null)
+            {
+                progressEvent(this, pe);
+            }
+        }
+
+
+        public void SlowMetthod()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                OnUpdateEvent();
+            }
+        }
+    }
+}
