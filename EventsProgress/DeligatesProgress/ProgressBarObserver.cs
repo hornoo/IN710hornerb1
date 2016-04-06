d9b17f0cf5e497078410e5b4f0848fb6735d2237
hornoo <rchrdhorne1@gmail.com>
2016-04-07 10:41:16 +1200

    progress and trackbar observers

---
+++ EventsProgress/DeligatesProgress/ProgressBarObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventsProgress
+{
+    public class ProgressBarObserver : TrackingObserver
+    {
+        ProgressBar inputProgressBar;
+
+
+        public ProgressBarObserver(SlowWorker Subject, ProgressBar InputProgressBar)
+            :base(Subject)
+        {
+            inputProgressBar = InputProgressBar;
+        }
+
+        public override void UpdateProgress(object o, EventArgs e)
+        {
+            inputProgressBar.PerformStep();
+        }
+    }
+}
---
+++ EventsProgress/DeligatesProgress/TrackbarObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventsProgress
+{
+    public class TrackbarObserver : TrackingObserver
+    {
+
+
+
+        public TrackbarObserver(SlowWorker Subject, TrackBar InputTrackBar):base(Subject)
+        {
+
+        }
+
+        public override void UpdateProgress(object o, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
