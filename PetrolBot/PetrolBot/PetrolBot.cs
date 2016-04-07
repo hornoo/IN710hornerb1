c4d9d57e1516626be3bc2e39ae2c7a371e1fb548
hornoo <rchrdhorne1@gmail.com>
2016-04-07 13:59:58 +1200

    defined bot class

--- PetrolBot/PetrolBot/PetrolBot.cs
+++ PetrolBot/PetrolBot/PetrolBot.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace PetrolBot
 {
    public class PetrolBot
     {
+
+       Graphics botCanvas;
+       Color botColour;
+       Point botCurrentlocation;
+       Ship botShip;
+       Point botStartingLocation;
+       Point shipLocation;
+
+
+       public PetrolBot()
+       {
+
+       }
+
+       public void drawBot()
+       {
+
+       }
+
+       public void FullOfFuelEventhandler()
+       {
+
+       }
+
+       public void OutOfFuelEventHandler()
+       {
+
+       }
+
+       
+       
     }
 }
