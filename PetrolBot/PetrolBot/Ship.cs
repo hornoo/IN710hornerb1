c2ce116e35eabf36347eee72ef7c132e732a9cd2
hornoo <rchrdhorne1@gmail.com>
2016-04-07 13:39:32 +1200

    added class skeletion

---
+++ PetrolBot/PetrolBot/Ship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrolBot
+{
+    enum EshipState { wandering, refueling};
+    public class Ship
+    {
+    }
+}
