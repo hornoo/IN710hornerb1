fbf2725a7ec9a16281ecec05ed4b3286037d9f37
hornoo <rchrdhorne1@gmail.com>
2016-04-07 13:47:06 +1200

    defined shipeventargs class

--- PetrolBot/PetrolBot/ShipEventArgs.cs
+++ PetrolBot/PetrolBot/ShipEventArgs.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace PetrolBot
 {
-   public class ShipEventArgs
+   public class ShipEventArgs : EventArgs
     {
+
+
+       public Point ShipLocation { get; set; }
+
+       public ShipEventArgs(Point coordonate)
+       {
+           ShipLocation = coordonate;
+       }
+
     }
 }
