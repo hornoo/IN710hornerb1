3935ea05552f5ec43df431cc330373a9ff3a4815
hornoo <rchrdhorne1@gmail.com>
2016-04-13 13:35:31 +1200

    City database , with city class created

---
+++ CityData/CityData/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityData
+{
+    public class City
+    {
+        public String CityName {get; set;}
+        public String CountryName {get; set;}
+
+        public int Population {get; set;}
+
+        public City(String cityName, String countryName, int population)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+            Population = population;
+        }
+    }
+}
