d326d4f73d83996d16fb7263d8ec5860f02ad013
hornoo <rchrdhorne1@gmail.com>
2016-04-13 14:18:47 +1200

    Basic City Data base complete unsinf func<T,T> to seatch

--- CityData/CityData/Form1.Designer.cs
+++ CityData/CityData/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btsearch = new System.Windows.Forms.Button();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btsearch
+            // 
+            this.btsearch.Location = new System.Drawing.Point(416, 52);
+            this.btsearch.Name = "btsearch";
+            this.btsearch.Size = new System.Drawing.Size(75, 23);
+            this.btsearch.TabIndex = 0;
+            this.btsearch.Text = "Search";
+            this.btsearch.UseVisualStyleBackColor = true;
+            this.btsearch.Click += new System.EventHandler(this.btsearch_Click);
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(153, 55);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(253, 20);
+            this.tbCity.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search for Cities of Country";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(479, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 227);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCity);
+            this.Controls.Add(this.btsearch);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btsearch;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- CityData/CityData/Form1.cs
+++ CityData/CityData/Form1.cs
@@ -12,9 +12,56 @@ namespace CityData
 {
     public partial class Form1 : Form
     {
+        List<City> cityList;
+
+        Func<string,List<City>,List<City>> findCitiesInCountry = (countryName, citylist) => (citylist.FindAll(City => City.CountryName == countryName));
+           
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cityList = new List<City>(){
+                new City("Dunedin", "New Zealand", 10000),
+                new City("Wanaka", "New Zealand", 5000),
+                new City("Milton", "New Zealand", 20),
+                new City("Sydney", "Australia", 4627345),
+                new City("Melborne", "Australia", 4246375),
+                new City("Brisbane", "Australia", 2189878),
+                new City("Perth", "Australia", 1896548),
+
+            };
+        }
+
+
+        private void searchCityInCountry()
+        {
+            String city = null;
+            List<City> returnList = new List<City>();
+            try
+            {
+                city = tbCity.Text;
+            }
+            catch
+            {
+            }
+
+            returnList = findCitiesInCountry(city, cityList);
+            
+            foreach(City currentCity in returnList)
+            {
+                listBox1.Items.Add(currentCity.CityName + " of " + currentCity.CountryName + " with a population of " + currentCity.Population);
+            }
+        }
+
+        private void btsearch_Click(object sender, EventArgs e)
+        {
+            searchCityInCountry();
+        }
+
     }
 }
