c4af9163f5b13ce1bd01eeaf8f52b81fb6b2a44f
hornoo <rchrdhorne1@gmail.com>
2016-05-11 14:50:20 +1200

    initial commit of ADO Practical

---
+++ ADOPractical/ADOPractical/DatabaseWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOPractical
+{
+    public class DatabaseWorker
+    {
+        SqlConnection bitDevConnection;
+
+
+        public DatabaseWorker()
+        {
+            bitDevConnection = new SqlConnection();
+        }
+
+        public void SetUpDatabase(String Username, String Password)
+        {
+            SetUpdatebaseConnection(Username, Password);
+
+
+
+        }
+
+        private void SetUpdatebaseConnection(String Username, String Password)
+        {
+            bitDevConnection.ConnectionString = "Data Source = bitdev.ivt.op.ac.nz;"+
+                                                "Initial Catalog = IN700001_201601_HORNERB1;" + 
+                                                "User ID = " + Username+ ";" +
+                                                "Password = " + Password + ";";
+
+            bitDevConnection.Open();
+        }
+    }
+}
---
+++ ADOPractical/ADOPractical/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ADOPractical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCreateTablesAddData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCreateTablesAddData
+            // 
+            this.btCreateTablesAddData.Location = new System.Drawing.Point(83, 128);
+            this.btCreateTablesAddData.Name = "btCreateTablesAddData";
+            this.btCreateTablesAddData.Size = new System.Drawing.Size(131, 23);
+            this.btCreateTablesAddData.TabIndex = 0;
+            this.btCreateTablesAddData.Text = "Create tables add data";
+            this.btCreateTablesAddData.UseVisualStyleBackColor = true;
+            this.btCreateTablesAddData.Click += new System.EventHandler(this.btCreateTablesAddData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(141, 54);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(100, 20);
+            this.tbUserName.TabIndex = 2;
+            this.tbUserName.Text = "hornerb1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(141, 84);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCreateTablesAddData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCreateTablesAddData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+    }
+}
+
